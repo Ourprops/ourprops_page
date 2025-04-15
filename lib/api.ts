206a6838b12291ad84c9caa72c053c50d83078aa
Joel Adjetey Komieter <komieterj@gmail.com>
2025-04-15 15:49:41 -0400

    Update the api endpoint

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4500' : "http://ourprops-core-api.ourprops.net",
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4500' : "http://api.ourprops.net",
 });
